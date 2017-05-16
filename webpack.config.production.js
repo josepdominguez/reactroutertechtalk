6e6c24d1796cd07cdd0d2e43567033d93b108346
jdominguez <jdominguez@dminc.com>
2017-05-16 13:46:25 +0700

    Initial commit

---
+++ webpack.config.production.js
@@ -0,0 +1,45 @@
+/* eslint-disable */
+
+var path = require("path");
+var webpack = require("webpack");
+
+module.exports = {
+  devtool: 'cheap-module-source-map',
+  entry: [
+    "babel-polyfill",
+    "./index"
+  ],
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/dist/"
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        "NODE_ENV": JSON.stringify("production")
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({ sourceMap: true }),
+    new webpack.optimize.AggressiveMergingPlugin()
+  ],
+  module: {
+    loaders: [{
+      test: /\.md$/,
+      loader: "html-loader!markdown-loader?gfm=false"
+    }, {
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      loader: "babel-loader"
+    }, {
+      test: /\.css$/,
+      loader: "style-loader!css-loader"
+    }, {
+      test: /\.(png|jpg|gif)$/,
+      loader: "url-loader?limit=8192"
+    }, {
+      test: /\.svg$/,
+      loader: "url-loader?limit=10000&mimetype=image/svg+xml"
+    }]
+  }
+};
